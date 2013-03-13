ff8574bc02b19a00ee45de02f210fb4a259adbdd
Rustam Irzaev <irzaevrustam@gmail.com>
2013-03-13 12:47:59 +0200

    Added BaseRequest

--- DisqusNET/Request/BaseRequest.cs
+++ DisqusNET/Request/BaseRequest.cs
@@ -1,11 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.IO;
+using System.Net;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace DisqusNET.Request
 {
     public class BaseRequest
     {
+        public enum HttpMethod { POST, GET };
+
+        public static HttpWebRequest BuildRequest(string url, HttpMethod method)
+        {
+            return BuildRequest(url, method, null);
+        }
+
+        public static HttpWebRequest BuildRequest(string url, HttpMethod httpMethod, MemoryStream content)
+        {
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = httpMethod.ToString();
+
+            if (content != null)
+            {
+                request.ContentLength = content.Length;
+                var requestStream = request.GetRequestStream();
+                requestStream.Write(content.ToArray(), 0, (int)content.Length);
+            }
+
+            return request;
+        }
+
+        public static MemoryStream SerializeContent<T>(T container) where T : class
+        {
+            var memoryStream = new MemoryStream();
+            var xmlTextWriter = new XmlTextWriter(memoryStream, null);
+
+            var namespaces = new XmlSerializerNamespaces();
+            namespaces.Add("", "");
+
+            var serializer = new XmlSerializer(typeof(T));
+            serializer.Serialize(xmlTextWriter, container, namespaces);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream;
+        }
     }
 }
